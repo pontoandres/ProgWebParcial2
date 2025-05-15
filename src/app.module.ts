bb6c786a5de4029cdc0fc0efc47dff7142a9ff0d
pontoandres <pa.morenot1@uniandes.edu.co>
2025-05-15 10:52:20 -0500

    final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { ProfesorModule } from './profesor/profesor.module';
+import { ProyectoModule } from './proyecto/proyecto.module';
+import { EvaluacionModule } from './evaluacion/evaluacion.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcialdb',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    EstudianteModule,
+    ProfesorModule,
+    ProyectoModule,
+    EvaluacionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
